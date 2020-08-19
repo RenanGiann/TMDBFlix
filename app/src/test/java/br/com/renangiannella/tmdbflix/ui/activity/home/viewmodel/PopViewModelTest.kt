cf00ecca42c89d43d4707daf027956cb69e9736b
Renan <renangiannella@gmail.com>
2020-08-19 14:25:41 -0300

    test SearchViewModel

---
+++ app/src/test/java/br/com/renangiannella/tmdbflix/ui/activity/home/viewmodel/PopViewModelTest.kt
@@ -0,0 +1,19 @@
+package br.com.renangiannella.tmdbflix.ui.activity.home.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class PopViewModelTest{
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun `when GetPopularMovie get success then set movieResponse` () {
+
+    }
+}
